3802a7d37a122deba98d9376bd11bf281b7354a3
Run Tao <Taoli_Run@outlook.com>
2023-10-22 22:52:20 +0800

    20231022

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 # request
-
+# for downloading pictures online
+# Purpose: to get the data which is formed in pictures
 import requests
 if __name__ == "__main__":
     url = "https://ss2.baidu.com/-vo3dSag_xI4khGko9WTAnF6hhy/baike/w=268/sign=b97505d9a2c27d1ea5263cc223d4adaf/4afbfbedab64034f15ba757ba9c379310a551d53.jpg"
